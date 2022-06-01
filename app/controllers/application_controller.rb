8e123a5d3f66bdc0b7dff0e4f3f08b82ce3ae8cf
Alexander Schuessler <schuessler.alex@gmail.com>
2022-06-01 09:34:41 -0500

    Added CRUD functionality to backend. Seperated controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,32 +9,4 @@ class ApplicationController < Sinatra::Base
     arr=[destinations,trips,stops]
     arr.to_json
   end
-
-  get "/destinations" do
-    destinations=Destination.all
-    destinations.to_json
-  end
-
-  get "/trips" do
-    trips = Trip.all
-    trips.to_json
-  end
-
-  get "/stops" do
-    stops = Stop.all
-    stops.to_json
-  end
-
-  get "/trips/:id" do
-    trip = Trip.find(params[:id])
-    trip.to_json(include: :destinations)
-  end
-
-  get "/destinations/:id" do
-    destination = Destination.find(params[:id])
-    destination.to_json(include: :trips)
-  end
-
-
-
 end
---
+++ app/controllers/stop_controller.rb
@@ -0,0 +1,41 @@
+class StopController < ApplicationController
+    get "/stops" do
+        stops = Stop.all
+        serialize(stops)
+    end
+
+    get "/stops/:id" do
+        stops = Stop.find(params[:id])
+        serialize(stops)
+    end
+
+    post "/stops" do
+        stop = Stop.create(
+            arrival: params[:arrival],
+            departure: params[:departure],
+            trip_id: params[:trip_id],
+            destination_id: params[:destination_id]
+        )
+        serialize(stop)
+    end
+
+    delete "/stops/:id" do
+        stop = Stop.find(params[:id])
+        stop.destroy
+        serialize(stop)
+    end
+
+    patch "/stops/:id" do
+        stop = Stop.update(
+            arrival: params[:arrival],
+            departure: params[:departure],
+            trip_id: params[:trip_id],
+            destination_id: params[:destination_id]
+        )
+        serialize(stop)
+    end
+
+    def serialize(stop)
+        stop.to_json
+    end
+end
